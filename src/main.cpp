f4ea6ec2c4d7be2e9385a82c2c568430acb84882
veasman <charlton.moren@gmail.com>
2021-09-27 17:39:49 -0500

    Add file input

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <string.h>
 
-int main() {
-    std::cout << "nums" << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        for (int i = 0; i < argc; i++) {
+            int len = strlen(argv[i]);
+            char* fileExt = &argv[i][len - 5];
+            if (strcmp(fileExt, ".nums") == 0) {
+                // Parser, lexer, etc...
+            }
+            else {
+                std::cout << "Error: File must end in \".nums\"" << std::endl;
+            }
+        }
+    }
+    else {
+        // Maybe live interpreter later?
+        std::cout << "Error: Please provide a file" << std::endl;
+        return 1;
+    }
 
     return 0;
 }
