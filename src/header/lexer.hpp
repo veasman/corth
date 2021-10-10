9bfad04da0eed254133828b3a4ea08344ef40f10
veasman <charlton.moren@gmail.com>
2021-10-10 10:23:11 -0500

    change token container to queue, cleanup main.cpp

--- src/header/lexer.hpp
+++ src/header/lexer.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include <vector>
 #include "position.hpp"
 #include "token.hpp"
 
@@ -7,7 +6,7 @@ class Lexer {
 public:
     std::string m_strFile;
     Position m_Pos = Position(-1, 0, -1); // Why do I need to initialize here?
-    std::vector<Token> m_vTokens; // TODO: Maybe this could be a queue (fifo)
+    std::queue<Token> m_qTokens;
     char m_cCur;
 
     Lexer(std::string file);
--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,70 @@
 #include <iostream>
 #include <string.h>
-#include <vector>
+#include <queue>
 #include "header/lexer.hpp"
 
+void PrintUsage() {
+    std::cout << "usage: ./corth file.corth" << std::endl;
+}
+
 int main(int argc, char* argv[]) {
-    if (argc > 1) {
-        for (int i = 1; i < argc; i++) {
-            int len = strlen(argv[i]);
-            char* fileExt = &argv[i][len - 6];
-
-            if (strcmp(fileExt, ".corth") == 0) {
-                Lexer lexer(argv[i]);
-                lexer.CreateTokens();
-
-                // Corth will do more later, but for now it will just show you
-                // the tokens created from a given file
-                for (Token token : lexer.m_vTokens) {
-                    std::string tokenString;
-                    switch (token.m_Type) {
-                    case TokenType_t::NUM:
-                        tokenString = "NUM";
-                        break;
-                    case TokenType_t::FLT:
-                        tokenString = "FLT";
-                        break;
-                    case TokenType_t::ADD:
-                        tokenString = "ADD";
-                        break;
-                    case TokenType_t::SUB:
-                        tokenString = "SUB";
-                        break;
-                    case TokenType_t::MUL:
-                        tokenString = "MUL";
-                        break;
-                    case TokenType_t::DIV:
-                        tokenString = "DIV";
-                        break;
-                    case TokenType_t::MOD:
-                        tokenString = "MOD";
-                        break;
-                    case TokenType_t::EQU:
-                        tokenString = "EQU";
-                        break;
-                    default:
-                        tokenString = "OTH";
-                        break;
-                    }
-                    std::cout <<
-                        "[" << tokenString <<
-                        ", " << token.m_strVal <<
-                        "]" << std::endl;
-                }
-            }
-            else {
-                std::cout << "Error: File must end in \".corth\"" << std::endl;
-            }
-        }
-    }
-    else {
-        // Maybe live interpreter later?
-        std::cout << "Error: Please provide a file" << std::endl;
+    if (argc < 2) {
+        PrintUsage();
         return 1;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        int len = strlen(argv[i]);
+        char* fileExt = &argv[i][len - 6];
+
+        if (strcmp(fileExt, ".corth") != 0) {
+            PrintUsage();
+            return 1;
+        }
+
+        Lexer lexer(argv[i]);
+        lexer.CreateTokens();
+
+        // Corth will do more later, but for now it will just show you
+        // the tokens created from a given file
+        for (int i = 0; i < lexer.m_qTokens.size(); i++) {
+            Token token = lexer.m_qTokens.front();
+            std::string tokenString;
+            switch (token.m_Type) {
+            case TokenType_t::NUM:
+                tokenString = "NUM";
+                break;
+            case TokenType_t::FLT:
+                tokenString = "FLT";
+                break;
+            case TokenType_t::ADD:
+                tokenString = "ADD";
+                break;
+            case TokenType_t::SUB:
+                tokenString = "SUB";
+                break;
+            case TokenType_t::MUL:
+                tokenString = "MUL";
+                break;
+            case TokenType_t::DIV:
+                tokenString = "DIV";
+                break;
+            case TokenType_t::MOD:
+                tokenString = "MOD";
+                break;
+            case TokenType_t::EQU:
+                tokenString = "EQU";
+                break;
+            default:
+                tokenString = "OTH";
+                break;
+            }
+            std::cout <<
+                "[" << tokenString <<
+                ", " << token.m_strVal <<
+                "]" << std::endl;
+            lexer.m_qTokens.pop();
+        }
     }
 
     return 0;
